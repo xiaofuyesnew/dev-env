a9757f630e542a5babcccd4008f5756560ec533d
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-03-31 10:58:10 +0800

    comments for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
+/**
+ * @filename    gulpfile.js
+ * 
+ * @create  2017-03-31
+ * 
+ * @author  Allen Wong
+ * 
+ * @description
+ *     gulpfile for gulp config
+ * 
+ * @update  2017-03-31
+ */
+
+
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
